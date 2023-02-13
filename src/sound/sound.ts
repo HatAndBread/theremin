3369bc31c4ae119613f22d4a1a3ebacb19635d4d
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-02-14 07:00:06 +0900

    Use x

--- src/sound/sound.ts
+++ src/sound/sound.ts
@@ -3,29 +3,21 @@ import { justIntonation } from "./just-intonation";
 import { equalTempered } from "./equal-tempered";
 
 
-const allInstruments: {sine: Tone.Oscillator, triangle: Tone.Oscillator, env: Tone.AmplitudeEnvelope}[] = []
+const allInstruments: {sine: Tone.Oscillator, triangle: Tone.Oscillator, env: Tone.AmplitudeEnvelope, gain: Tone.Gain, sampler: Tone.Sampler}[] = []
 for (let i = 0; i < 5; i++) {
+  const gain = new Tone.Gain(0).toDestination()
   const env = new Tone.AmplitudeEnvelope({
     attack: 0.1,
     decay: 0.2,
     sustain: 0.5,
     release: 0.8,
-  }).toDestination();
+  }).connect(gain);
   const sine = new Tone.Oscillator({type: "sine"}).connect(env).start()
   const triangle = new Tone.Oscillator({type: "triangle"}).connect(env).start()
-  allInstruments.push({sine, triangle, env})
+  const sampler = new Tone.Sampler().toDestination();
+  allInstruments.push({sine, triangle, env, gain, sampler})
 }
-const env = new Tone.AmplitudeEnvelope({
-  attack: 0.1,
-  decay: 0.2,
-  sustain: 0.5,
-  release: 0.8,
-}).toDestination();
-const sine = new Tone.Oscillator({type: "sine"}).connect(env).start()
-const triangle = new Tone.Oscillator({type: "triangle"}).connect(env).start()
-const sampler = new Tone.Sampler().toDestination();
 
-const instruments: Tone.Oscillator[] = [triangle, sine]
 const startFrequency = 132;
 const scale = justIntonation;
 
@@ -37,10 +29,8 @@ const updateFrequency = (frequency: number, touchNumber: number) => {
   allInstruments[touchNumber].triangle.frequency.value = frequency;
 }
 
-const updateVolume = (force: number, touchNumber: number) => {
-  const newValue = -115 + (force * 10)
-  allInstruments[touchNumber].sine.volume.value = 0;
-  allInstruments[touchNumber].triangle.volume.value = 0;
+const updateVolume = (volume: number, touchNumber: number) => {
+  allInstruments[touchNumber].gain.gain.value = volume;
 }
 
 function start(touchNumber: number) {
@@ -51,14 +41,14 @@ function stop(touchNumber: number) {
   allInstruments[touchNumber].env.triggerRelease(Tone.now())
 }
 
-function play (noteNumber: number, offset: number, touchNumber: number, force: number) {
+function play (noteNumber: number, offset: number, touchNumber: number, volume: number) {
   if (!window.started) return;
   const baseFrequency = frequencyForNoteNumber(noteNumber);
   const nextFrequency = frequencyForNoteNumber(noteNumber + 1);
   const diff = nextFrequency - baseFrequency
   const frequency = baseFrequency + (diff * offset)
 
-  updateVolume(force, touchNumber);
+  updateVolume(volume, touchNumber);
   updateFrequency(frequency, touchNumber);
 }
 
