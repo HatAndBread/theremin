e56f8cfc735c04e5435eba1ba965435a3de02e07
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2023-02-21 08:31:43 +0900

    Set frequency back

--- src/sound/effect-adjusters.ts
+++ src/sound/effect-adjusters.ts
@@ -19,7 +19,7 @@ export const setEffectAdjusters = (name: EffectNames, values: {x: number, y: num
     e.wet.value = y;
   } else if (name === "shift") {
     const e = effect as FrequencyShifter;
-    e.frequency.rampTo(x * 500, 0.1)
+    e.frequency.rampTo(x * 120, 0.1)
     e.wet.value = y;
   } else {
     throw new Error("Unimplemented effect")
